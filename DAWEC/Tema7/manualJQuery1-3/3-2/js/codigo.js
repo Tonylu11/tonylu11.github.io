236339619f8075bf75a90777814ba235b13a6721
Antonio Luque Bravo <toniluque097@gmail.com>
2017-01-11 23:18:50 +0100

    Actualización JQuery Parte 1-3

--- DAWEC/Tema7/manualJQuery1-3/3-2/js/codigo.js
+++ DAWEC/Tema7/manualJQuery1-3/3-2/js/codigo.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
 	$("#boton").click(function(evento){
-		let selectorId = $("#camposelector").prop("value");
-		let selectorClase = $(".clasecamposelector").prop("value");
-		let selectorElemento = $("p").prop("value");
-		let selectorVariasClases = $(".clase1.clase2").prop("value");
+		let selectorEscrito = $("#camposelector").prop("value");
 		if(selectorEscrito == ""){
 			alert("Escribe algo en el campo de texto");
 		}else{
